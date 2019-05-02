1759e8e582dee17b3813ca7b505e21b022b1af67
Conor <conor@wilxite.com>
2019-05-02 14:23:35 +0100

    apollo client installed, layouts sass file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-memory'
+
 import Header from './components/Header'
 import { Sidebar, SidebarItem } from './components/Sidebar'
 import Article from './components/Article'
 
 import './assets/styles/_globals.sass'
 
-const API_URL = ""
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+})
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
 
 class App extends Component {
 
@@ -16,39 +27,42 @@ class App extends Component {
     }
 
     componentDidMount() {
-        fetch(API_URL)
-            .then(res => res.json())
-            .then(articles => {
-                this.setState({ articles })
-            })
+        // fetch(API_URL)
+        //     .then(res => res.json())
+        //     .then(articles => {
+        //         this.setState({ articles })
+        //     })
     }
 
     render() {
         const { articles } = this.state
         return (
             <Router>
-                <div>
-                    <Header />
-                    <Sidebar>
-                        { articles ? (
-                            articles.map(article => (
-                                <SidebarItem key={ article.id }>
-                                    <Link to={ `/a/${article.id}` }>
-                                        { article.title }
-                                    </Link>
-                                </SidebarItem>
-                            ))
-                        ) : (
-                            <div>Loading...</div>
-                        )}
-                    </Sidebar>
-                    <main>
-                        <Route path="/g/:articleId" component={ Article } />
-                    </main>
-                </div>
+                <Header />
+                <Sidebar>
+                    { articles ? (
+                        articles.map(article => (
+                            <SidebarItem key={ article.id }>
+                                <Link to={ `/a/${article.id}` }>
+                                    { article.title }
+                                </Link>
+                            </SidebarItem>
+                        ))
+                    ) : (
+                        <div>Loading...</div>
+                    )}
+                </Sidebar>
+                <main>
+                    <Route path="/g/:articleId" component={ Article } />
+                </main>
             </Router>
         )
     }
 }
 
-ReactDOM.render(<App />, document.getElementById("content"))
+ReactDOM.render(
+    <ApolloClient client={ client }>
+        <App />
+    </ApolloClient>,
+    document.getElementById("content")
+)
